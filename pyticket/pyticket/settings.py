e759db6811caec3fbe0d50018f995e34b323646a
Daniel <daniel@becher.com.br>
2022-03-14 15:54:43 -0300

    ajustes!

--- pyticket/pyticket/settings.py
+++ pyticket/pyticket/settings.py
@@ -117,10 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = '/home/pyticket/pyticket/pyticket/static/'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
-STATICFILES_DIRS = [BASE_DIR / 'static']     
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
