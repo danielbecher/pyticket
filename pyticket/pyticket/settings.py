ee083c6e5fe0eb2b6ad08ef3e5490f3ba907813b
Daniel <daniel@becher.com.br>
2022-03-14 15:37:43 -0300

    ajustes!

--- pyticket/pyticket/settings.py
+++ pyticket/pyticket/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@&y)x7j#v&lw$n_j=$c$!+vb0cbij7iu^zav=u!*s$ddlkwnj0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['IP', 'http://pyticket.becher.com.br','127.0.0.1']
+ALLOWED_HOSTS = ['IP', 'http://pyticket.becher.com.br']
 CSRF_TRUSTED_ORIGINS = ['http://pyticket.becher.com.br']
 
 # Application definition
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATICFILES_DIRS = [BASE_DIR / 'static']     
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
