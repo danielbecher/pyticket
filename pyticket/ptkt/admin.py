5189eea97e8572d84d86f2c8cd1474d9f6dd430d
Daniel <daniel@becher.com.br>
2022-03-15 09:21:46 -0300

    Criada a view para acesso aos dados de email ao DB

--- pyticket/ptkt/admin.py
+++ pyticket/ptkt/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from ptkt.models import Tickets, Interacoes
+from pyticket.ptkt.models import Notificacoes
 
 class ticketsAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'assunto', 'status', 'prioridade', 'data_abertura')
@@ -7,5 +8,9 @@ class ticketsAdmin(admin.ModelAdmin):
 class interacoesAdmin(admin.ModelAdmin):
     list_display = ('id_usuario','data_criacao', 'interacao', 'chamado_id')
 
+class notificacoesAdmin(admin.ModelAdmin):
+    list_display = ('host','porta','usuario','senha')
+
 admin.site.register(Tickets, ticketsAdmin)
 admin.site.register(Interacoes, interacoesAdmin)
+admin.site.register(Notificacoes, notificacoesAdmin)
